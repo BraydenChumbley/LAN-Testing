8b0f45e0e1b3204da77e71c94d276d06b131ec08
Brayden Chumbley <bchumbley2001@gmail.com>
2019-05-17 20:29:15 -0400

    Added Server and Client classes

--- LANServerTesting/src/bchum/LANTest/Client.java
+++ LANServerTesting/src/bchum/LANTest/Client.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bchum.LANTest;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Brayden Chumbley
  */
-public class Client {
-    
+public class Client 
+{
+	public Client(String address, int port, String userName)
+	{
+		try
+		{
+			System.out.println("Attempting to connect to: " + address + ":" + port);
+			Socket client = new Socket(address, port);
+			
+			System.out.println("Connection Aquired!");
+			DataOutputStream outStream = new DataOutputStream(client.getOutputStream());
+			outStream.writeUTF("Hello, my name is " + userName);
+			
+			DataInputStream inStream = new DataInputStream(client.getInputStream());
+			System.out.println("SERVER: " + inStream.readUTF());
+			
+			client.close();
+		} 
+		catch (IOException ex) 
+		{
+			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	public static void main(String[] args) throws UnknownHostException
+	{
+		if(args.length < 0)
+		{
+			System.out.println("Arguments required: <address> <port> <user_name>");
+		}
+		Client c = new Client(args[0], Integer.parseInt(args[1]), args[2]);
+	}
 }
--- LANServerTesting/src/bchum/LANTest/Server.java
+++ LANServerTesting/src/bchum/LANTest/Server.java
@@ -5,10 +5,88 @@
  */
 package bchum.LANTest;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Brayden Chumbley
  */
-public class Server {
-    
+public class Server extends Thread
+{
+	
+	private ServerSocket ss;
+	
+	public Server(int port) throws IOException
+	{
+		ss = new ServerSocket(port, 1, InetAddress.getLocalHost());
+		ss.setSoTimeout(10000);
+	}
+	
+	@Override
+	public void run()
+	{
+		Scanner stdInput = new Scanner(System.in);
+		String continueServer;
+		
+		System.out.print("Start server? (Y/N): ");
+		
+		while((continueServer = stdInput.nextLine()).equals("Y") || continueServer.equals("y"))
+		{
+			try
+			{
+				System.out.println("Waiting for client at: " + ss.getInetAddress().getCanonicalHostName() + ":" + ss.getLocalPort());
+				Socket socket = ss.accept();
+				
+				System.out.println("Connection Aquired!");
+				DataInputStream inStream = new DataInputStream(socket.getInputStream());
+				System.out.println("CLIENT: " + inStream.readUTF());
+				
+				DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+				outStream.writeUTF("Thank you for connecting! Goodbye!");
+				
+				socket.close();
+			}
+			catch (SocketTimeoutException ex)
+			{
+				System.out.println("Socket Timed Out");
+			}
+			catch (IOException ex) 
+			{
+				Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			
+			System.out.print("Wait for another client? (Y/N): ");
+		}
+	}
+	
+    public static void main(String[] args)
+	{
+		if(args.length < 1)
+		{
+			System.out.println("Arguments required: <port>");
+		}
+		
+		try
+		{
+			Thread t = new Server(Integer.parseInt(args[0]));
+			t.start();
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Invalid port");
+		} 
+		catch (IOException ex) 
+		{
+			Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
